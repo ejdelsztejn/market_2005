7542bb3c7c95abf69f45183d574ce91fe7219747
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-06-10 11:25:37 -0400

    Add market test it has attributes

--- test/market_test.rb
+++ test/market_test.rb
@@ -6,6 +6,13 @@ class MarketTest < MiniTest::Test
   def test_it_exists
     market = Market.new("South Pearl Street Farmers Market")
 
-    assert_instance_of Market, market  
+    assert_instance_of Market, market
+  end
+
+  def test_it_has_attributes
+    market = Market.new("South Pearl Street Farmers Market")
+
+    assert_equal "South Pearl Street Farmers Market", market.name
+    assert_equal [], market.vendors
   end
 end

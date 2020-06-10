bf698b97cd808aa7bdd2407eb24fbbcccebfdba7
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-06-10 11:14:45 -0400

    Change test it has name t test it has attributes

--- test/vendor_test.rb
+++ test/vendor_test.rb
@@ -9,9 +9,10 @@ class VendorTest < MiniTest::Test
     assert_instance_of Vendor, vendor
   end
 
-  def test_it_has_a_name
+  def test_it_has_attributes
     vendor = Vendor.new("Rocky Mountain Fresh")
 
     assert_equal "Rocky Mountain Fresh", vendor.name
+    assert_equal ({}), vendor.inventory
   end
 end

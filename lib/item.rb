632a79bbe6664a3f81e4bd134d48cf122e04edac
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-06-10 11:07:48 -0400

    Add item attributes name and price

--- lib/item.rb
+++ lib/item.rb
@@ -1,5 +1,8 @@
 class Item
+  attr_reader :name,
+              :price
   def initialize(info_hash)
-    @info_hash = info_hash
+    @name = info_hash[:name]
+    @price = info_hash[:price].delete("$").to_f
   end
 end

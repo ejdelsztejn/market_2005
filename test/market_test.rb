8032c9663d51559edf57fce263cb9f95298c3761
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-06-10 11:24:03 -0400

    Add market test and test it exists

---
+++ test/market_test.rb
@@ -0,0 +1,11 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/market'
+
+class MarketTest < MiniTest::Test
+  def test_it_exists
+    market = Market.new("South Pearl Street Farmers Market")
+
+    assert_instance_of Market, market  
+  end
+end

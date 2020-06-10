5737fdd7091712471c49b649dc43a39905385f80
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-06-10 11:06:44 -0400

    Add test it has attributes

--- test/item_test.rb
+++ test/item_test.rb
@@ -8,4 +8,11 @@ class ItemTest < MiniTest::Test
 
     assert_instance_of Item, item1
   end
+
+  def test_it_has_attributes
+    item1 = Item.new({name: 'Peach', price: "$0.75"})
+
+    assert_equal "Peach", item1.name
+    assert_equal 0.75, item1.price
+  end
 end
